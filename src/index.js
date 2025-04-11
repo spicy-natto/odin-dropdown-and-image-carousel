0f6d0f6cea952ba16afd89cc5a4124b355925a74
Jeff Leep <jdleep@protonmail.com>
2025-04-11 17:33:25 -0500

    Create function to generate image element array
    Begin writing code to create nav circles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./css/styles.css";
+import carousel from "./modules/carousel.js";
 import addClickEventToAllDropdowns from "./modules/dropdown.js";
 
 addClickEventToAllDropdowns();
+carousel.createImgElementArray();
--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,8 +1,48 @@
-// Taken from https://webpack.js.org/guides/dependency-management/
-const imageCache = {};
+import deer from "../images/deer.jpg";
+import funnyCow from "../images/funny-cow-picture.jpg";
+import lake from "../images/lake.jpg";
+import path from "../images/path.jpg";
 
-function importAll(r) {
-  r.keys().forEach((key) => (cache[key] = r(key)));
+const imagesWithAltText = [
+  { image: deer, altText: "Deer" },
+  { image: funnyCow, altText: "Funny Cow" },
+  { image: lake, altText: "lake" },
+  { image: path, altText: "path" },
+];
+
+class Carousel {
+  carouselDiv = document.getElementById("carousel");
+  navCirclesDiv = document.getElementById("nav-circles");
+  currIndex = 0;
+  imageDomArray;
+  navCircleArray;
+
+  constructor(imageAltMapping) {
+    this.imageAltMapping = imageAltMapping;
+  }
+
+  createImgElementArray() {
+    this.imageDomArray = this.imageAltMapping.map((imageAlt) =>
+      this.#createImgElement(imageAlt)
+    );
+    console.log(this.imageDomArray);
+  }
+
+  #createImgElement({ image, altText }) {
+    const img = document.createElement("img");
+    img.setAttribute("src", image);
+    img.setAttribute("alt", altText);
+    return img;
+  }
+
+  #createNavCircleElement({id, value}) {
+    const radioButton = document.createElement("input");
+    radioButton.setAttribute("type", "radio");
+    radioButton.setAttribute("name", "image-select");
+    radioButton.setAttribute("id", id);
+    radioButton.setAttribute("value", value);
+    return radioButton;
+  }
 }
 
-importAll(require.context("../images/", false, /\.jpg$/));
+export default new Carousel(imagesWithAltText);
